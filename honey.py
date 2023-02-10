9da1cf9898e330a593335fc39e6746debfbcd03e
Future <72302164+giachad@users.noreply.github.com>
2023-02-10 13:11:53 +0000

    Downgraded for stability

--- honey.py
+++ honey.py
@@ -1,66 +1,27 @@
+import requests
 import json
 import os
 
 tags = {"script": "sc", "python": "py", "honey": "ho"}
 
+variables = {}
+functions = {}
+
 def honeyError(what):
      #print(f"[{tags['honey']}] {what}\n     please open a issue here:\n     github.com/giachad/honey/issues")
      print(f"[{tags['honey']}] {what}")
 
-def checkValue(self, what_):
-    print(what_)
-    if str(what_).startswith('"'):
-        return str(what_.lstrip('"').rstrip('"'))
-    what_ = str(what_)
-    if isNumberic(what_):
-        return float(what_)
-    elif self.variables.get(str(what_)):
-        return self.variables[what_]
-    elif "*" in str(what_) or "+" in str(what_) or "/" in str(what_) or "-" in str(what_):
-        try:
-            newWhat = self.forVariables(what_)
-            return eval(str(newWhat))
-        except:
-            pass
-    elif isJson(what_):
-        return json.loads(what_)
-    else:
-        directory = self.valuePathToFilePath(str(what_).split("[")[0])
-        if directory != None:
-            if what_.endswith("]"):
-                indexed = checkValue(self, what_.split("[")[1].rstrip("]"))
-                tempFile = HoneyFile(directory)
-
-                for variable in tempFile.variables:
-                    if variable == tempFile:
-                        return tempFile.variables[variable]
-        else:
-            return "nil"
-                
-def isNumberic(what):
-    try:
-        float(what)
-        return True
-    except:
-        return False
-
-def isJson(what):
-    try:
-        json.loads(what)
-        return True
-    except:
-        return False
+def readlines(where):
+    f = open(where, "r")
+    c = f.readlines()
+    f.close()
+    return c
 
 class Script(object):
-    def __init__(self, location) -> None:
-        self.location = location
-        self.code = ""
-        self.variables = {}
-        self.functions = {}
-
-        self.file = open(self.location, "r")
-        self.code = self.file.readlines()
-        self.file.close()
+    def __init__(self, code) -> None:
+        self.code = code
+        variables = {}
+        functions = {}
 
         self.manageFunctions()
         self.manageVariables()
@@ -75,23 +36,28 @@ class Script(object):
                 parametresString = line.rstrip("\n").split("(")[1].rstrip(") {")
                 parametres = parametresString.split(", ")
                 currentFunctionName = line.split(" ")[1].rstrip(") {").rstrip(parametresString).rstrip("(")
-                self.functions[currentFunctionName] = {"parametres": parametres, "lines": []}
+                functions[currentFunctionName] = {"parametres": parametres, "lines": [], "return": None}
             elif "}" in line:
                 currentFunctionName = ""
             else:
                 if currentFunctionName != "":
-                    self.functions[currentFunctionName]["lines"].append(line.lstrip(" ").rstrip("\n"))
+                    if line.lstrip(" ").rstrip("\n").startswith("return"):
+                        print(self.getValueFromBrackets(line.lstrip(" ").rstrip("\n")))
 
+                        functions[currentFunctionName]["return"] = self.getValueFromBrackets(line.lstrip(" ").rstrip("\n"))
+
+                    functions[currentFunctionName]["lines"].append(line.lstrip(" ").rstrip("\n"))    
+                    
     def manageVariables(self):
         for line in self.code:
             if line.startswith("local"):
                 variableName = line.split(" ")[1]
 
                 if len(line.split(" ")) >= 3 and line.split(" ")[2] == "=":
-                    variableValue = checkValue(self, line.lstrip(line.split(" = ")[0] + " = ").rstrip("\n"))
-                    self.variables[variableName] = variableValue
+                    variableValue = self.checkValue(line.lstrip(line.split(" = ")[0] + " = ").rstrip("\n"))
+                    variables[variableName] = variableValue
                 else:
-                    self.variables[variableName] = None
+                    variables[variableName] = None
 
     def valuePathToFilePath(self, what):
         returnString = ""
@@ -113,8 +79,8 @@ class Script(object):
     def forVariables(self, what):
         newWhat = str(what)
 
-        for variable in self.variables:
-            newWhat = newWhat.replace(variable, str(self.variables[variable]))
+        for variable in variables:
+            newWhat = newWhat.replace(variable, str(variables[variable]))
 
         return str(newWhat)
 
@@ -122,12 +88,19 @@ class Script(object):
         if os.path.exists(where):
             os.startfile(where, "print")
 
+    def getValueFromBrackets(self, line):
+        if "(" in line:
+            line = line.split("(")[1]
+            if ")" in line:
+                line = line.rstrip(")\n")
+                return self.checkValue(line)
+
     def getValuesFromBrackets(self, line):
         if "(" in line:
             line = line.split("(")[1]
             if ")" in line:
                 line = line.rstrip(")\n")
-                return checkValue(self, line)
+                return self.checkValue(line.split(", "))
 
     def runCode(self, theCode):
         currentLine = 0
@@ -136,137 +109,66 @@ class Script(object):
             currentLine += 1
 
             if line.startswith("echo"):
-                print(f"[{tags['script']}] "+str(self.getValuesFromBrackets(line)))
-            elif self.functions.get(str(line).split("(")[0].rstrip("\n")):
+                print(f"[{tags['script']}] "+str(self.getValueFromBrackets(line)))
+
+            elif line.startswith("import"):
+                where = self.getValueFromBrackets(line)
+
+                if os.path.isfile(where+".honey"):
+                    importedScript = Script(where+"honey")
+                elif os.path.isfile(where):
+                    importedScript = Script(where)
+                elif self.isUrl(where):
+                    importedScriptCode = str(requests.get(where).text).rstrip("\n").splitlines()
+                    importedScript = Script(importedScriptCode)
+                    
+            elif functions.get(str(line).split("(")[0].rstrip("\n")):
                 functionName = str(line).split("(")[0].rstrip("\n")
-                
+
                 for index, parametre in enumerate(str(line).split("(")[1].rstrip(")").split(", ")):
                     try:
-                        self.variables[self.functions[functionName]["parametres"][index]] = checkValue(self, parametre)
+                        variables[functions[functionName]["parametres"][index]] = self.checkValue(parametre)
                     except:
                         honeyError("no parametre of index "+str(index))
 
-                self.runCode(self.functions[functionName]["lines"])
+                self.runCode(functions[functionName]["lines"])
 
-class File:
-    def __init__(self, location) -> None:
-        self.location = str(location)
-        self.data = {}
+    def checkValue(self, what_):
+        try:
+            if str(what_).startswith('"'):
+                return str(what_.lstrip('"').rstrip('"'))
 
-        if self.location.endswith(".honey"):
-            try:
-                self.__class__ = HoneyFile
-                self.__init__(location)
-            except:
-                honeyError("problem registering a honey script")
-        elif self.location.endswith(".json"):
-            f = open(self.location, "r")
-            self.data = json.loads(f.read())
-            f.close()
-            print(self.data)
+            what_ = str(what_)
 
-class HoneyFile(object):
-    def __init__(self, location) -> None:
-        self.location = location
-        self.code = ""
-        self.variables = {}
-        self.functions = {}
-
-        self.file = open(self.location, "r")
-        self.code = self.file.readlines()
-        self.file.close()
-
-        self.manageFunctions()
-        self.manageVariables()
-
-    def manageFunctions(self):
-        currentFunctionName = ""
-
-        for line in self.code:
-            if "function" in line:
-                parametresString = line.rstrip("\n").split("(")[1].rstrip(") {")
-                parametres = parametresString.split(", ")
-                currentFunctionName = line.split(" ")[1].rstrip(") {").rstrip(parametresString).rstrip("(")
-                self.functions[currentFunctionName] = {"parametres": parametres, "lines": []}
-            elif "}" in line:
-                currentFunctionName = ""
+            if self.isNumberic(what_):
+                return float(what_)
+            elif variables.get(str(what_)):
+                return variables[what_]
+            elif "*" in str(what_) or "+" in str(what_) or "/" in str(what_) or "-" in str(what_):
+                try:
+                    newWhat = self.forVariables(what_)
+                    return eval(str(newWhat))
+                except:
+                    pass
+            elif self.isJson(what_):
+                return json.loads(what_)
+            elif functions.get(what_.split("(")[0]):
+                print(what_.split("(")[0].rstrip("\n"))
+                newnewWhat = what_.split("(")[0]
+                if functions[newnewWhat]["return"] != None:
+                    return functions[what_.split("(")[0]]["return"]
             else:
-                if currentFunctionName != "":
-                    self.functions[currentFunctionName]["lines"].append(line.lstrip(" ").rstrip("\n"))
-
-    def manageVariables(self):
-        for line in self.code:
-            if line.startswith("local"):
-                variableName = line.split(" ")[1]
-
-                if len(line.split(" ")) >= 3 and line.split(" ")[2] == "=":
-                    variableValue = checkValue(self, line.lstrip(line.split(" = ")[0] + " = ").rstrip("\n"))
-                    self.variables[variableName] = variableValue
-                else:
-                    self.variables[variableName] = None
-                
+                return "nil"
+        except Exception as err:
+            honeyError(str(err))
+                    
     def isNumberic(self, what):
         try:
             float(what)
             return True
         except:
             return False
-
-    def valuePathToFilePath(self, what):
-        returnString = ""
-
-        for index, thing in enumerate(str(what).split(".")):
-            if index != (len(str(what).split(".")) - 1):
-                returnString += "/" +str(thing)
-            else:
-                returnString += "." + thing
-
-        returnString = returnString.lstrip("/")
-
-        if os.path.exists(returnString):
-            return returnString
-        else:
-            honeyError("file does not exist")
-            return None
-
-    def forVariables(self, what):
-        newWhat = str(what)
-
-        for variable in self.variables:
-            newWhat = newWhat.replace(variable, str(self.variables[variable]))
-
-        return str(newWhat)
-
-    def printerPrint(self, where):
-        if os.path.exists(where):
-            os.startfile(where, "print")
-
-    def getValuesFromBrackets(self, line):
-        if "(" in line:
-            line = line.split("(")[1]
-            if ")" in line:
-                line = line.rstrip(")\n")
-                return checkValue(self, line)
-
-    def runCode(self, theCode):
-        currentLine = 0
-
-        for line in theCode:
-            currentLine += 1
-
-            if line.startswith("echo"):
-                print(f"[{tags['script']}] "+str(self.getValuesFromBrackets(line)))
-            elif self.functions.get(str(line).split("(")[0].rstrip("\n")):
-                functionName = str(line).split("(")[0].rstrip("\n")
-                
-                for index, parametre in enumerate(str(line).split("(")[1].rstrip(")").split(", ")):
-                    try:
-                        self.variables[self.functions[functionName]["parametres"][index]] = checkValue(self, parametre)
-                    except:
-                        honeyError("no parametre of index "+str(index))
-
-                self.runCode(self.functions[functionName]["lines"])
-
+    
     def isJson(self, what):
         try:
             json.loads(what)
@@ -274,4 +176,13 @@ class HoneyFile(object):
         except:
             return False
 
-script = Script("script")
+    def isUrl(self, url):
+        response = requests.get(str(url))
+        try:
+            _ = response.text
+            return True
+        except:
+            return False
+
+
+script = Script(readlines("script.honey"))
